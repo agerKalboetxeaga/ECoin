2828c944c88e547f99142b57f298c1471f0678c7
agerKalboetxeaga <75113781+agerKalboetxeaga@users.noreply.github.com>
2022-04-25 20:44:37 +0200

    Pruebas con la api y JS y HTML

--- Web/Prueba/index.js
+++ Web/Prueba/index.js
@@ -4,11 +4,13 @@ const API_URL = 'http://127.0.0.1:5000';  //Ruta al Servidor
 
 const HTMLResponse = document.querySelector("#app");
 
-
+//El fetch te devuelve promises (que es como un async task) then es cuando tiene respuesta de esa url y podemos convertirlo en json y luego al hacer map, iterar por cada objeto dentro del array (cryptos)
 fetch(`${API_URL}/vcrypto`)
 .then((response) => response.json())
 .then((cryptos) => {
-    const template = cryptos.map((crypto) => `<li>${crypto.Crypto_Name} => ${crypto.Crypto_Price}€ \n <br>date: ${crypto.Query_Date}</li>`);
+    const template = cryptos.map((crypto) => `<li>${crypto.Crypto_Name} => ${crypto.Crypto_Price}€ \n <br>date: ${crypto.Query_Date}</li>`);   // las `` sirven pa meter variables pam, èro taen se puede:  "<li>", crypto.Crypto_Name, " => ", crypto.Crypto_Price, "\n", "<br>date: ", crypto.Query_Date, "</li>";
 
     HTMLResponse.innerHTML = `<ul>${template}</ul>`
 }).catch((err) => alert(`Ha habido un error conectandose al servidor: ${API_URL} \n Error: ${err.message}`));
+
+
