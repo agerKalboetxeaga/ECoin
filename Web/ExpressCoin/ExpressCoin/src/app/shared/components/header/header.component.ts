d092c3241c17d341264f1dfc70a04b727d21ac62
Ager Kalboetxeaga <a.kalboetxeaga@kirol.es>
2022-05-13 12:03:17 +0200

    si inicias sesion desaparece register y login te lleva a user-management

--- Web/ExpressCoin/ExpressCoin/src/app/shared/components/header/header.component.ts
+++ Web/ExpressCoin/ExpressCoin/src/app/shared/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { RunEXECommand } from 'src/app/models/runExecommand.model';
 import { CryptoService } from '../services/cryptoService.service';
+import { LoginService } from '../services/loginService.service';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +12,32 @@ import { CryptoService } from '../services/cryptoService.service';
 export class HeaderComponent implements OnInit {
 
   command : RunEXECommand = new RunEXECommand("");
-  constructor(private cryptoSvc : CryptoService) { }
+  constructor(private cryptoSvc : CryptoService,
+    private loginSvc: LoginService,
+    private cookieSvc : CookieService) { }
 
   ngOnInit(): void {
+  
+    let _register = (<HTMLElement> document.getElementById("_registrar"));
+    let _login = (<HTMLAreaElement> document.getElementById("_login"));
+    let token = this.cookieSvc.get("token");
+    console.log("no puede ser: " + token)
+    if(token!= null || token!= undefined || token === ""){
+      console.log("polla");
+      
+      _register.style.display = "none";
+      _register.style.visibility = "invisible";
+      _login.textContent="User";
+      _login.href="/user-management";
+  
+
+    }if(token == ""){
+      console.log("entra?")
+      _register.style.display = "flex";
+      _register.style.visibility = "visible";
+      _login.textContent="Login";
+      _login.href="/login";
+    }
   }
 
   startEXE(){
@@ -26,5 +51,24 @@ export class HeaderComponent implements OnInit {
     this.command.command = "stop"
     this.cryptoSvc.startEXE(this.command).subscribe(x => alert("Crypto price updater suspended!"));
   }
+  fallbackToStore(){
+    window.location.replace('market://details?id=com.express.Express-Coin');
+
+  }
+
+  openApp(){
+    window.location.replace('http://');
+    this.updateHeader();
+
+  }
+  triggerAppOpen(){
+    this.openApp();
+    setTimeout(this.fallbackToStore, 250);
+  }
+  updateHeader(){
+    
+  }
+
+  
 
 }
